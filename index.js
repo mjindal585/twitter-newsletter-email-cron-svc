e7baeaa91786ddb5c8043a871ed79897ef5e3f80
mjindal585 <jindalmohit64@gmail.com>
2023-01-29 14:55:11 +0530

    feat: cron setup

--- index.js
+++ index.js
@@ -3,70 +3,70 @@ const cron = require('cron');
 const MongoClient = require('mongodb').MongoClient;
 const nodemailer = require('nodemailer');
 
+require('dotenv').config();
 
-// Replace with your MongoDB connection string
-const url = 'local';
-const dbName = 'Users';
-const collectionName = 'subscribers';
+const mongodb_url = process.env.MONGO_URL;
+const mailFrom = process.env.MAIL_FROM;
+const mailPass = process.env.MAIL_FROM_PASSWORD;
+const dbName = process.env.DB_NAME;
+const collectionName = process.env.COLLECTION_NAME;
 
-// Replace with your email credentials
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'abcd@gmail.com',
-        pass: 'abcd@1'
-    },
-    tls: {
-        rejectUnauthorized: false
-    },
-});
+// Replace with the API endpoint for your data
+const twitterDataPullAPI = process.env.TWITTER_DATA_PULL_SVC_BASE_URL + '/get_sentiments';
 
-// Replace with the API endpoint for weather data
-const weatherApi = 'locals';
-
-// Fetch the weather data from the API
-const fetchWeatherData = async (query) => {
-    const response = await axios.post(weatherApi, { query, count: 1000 });
-    console.log('response --- ', response)
+// Fetch the tweetData data from the API
+const fetchTwitterData = async (query) => {
+    const response = await axios.post(twitterDataPullAPI, { query, count: 1000 });
     return response.data;
 }
 
-// Send an email to the user with the weather data
-const sendWeatherEmail = async (user) => {
-    const weatherData = await fetchWeatherData(user.category);
+// Send an email to the user with the tweetData data
+const sendNewsletterEmail = async (user) => {
+    const { category } = user;
+    const tweetData = await fetchTwitterData(category);
     const mailOptions = {
-        from: 'user@example.com',
+        from: mailFrom,
         to: user.email,
-        subject: 'Weather Report',
-        text: `${JSON.stringify(weatherData, null, 4)}.`
+        subject: `${String(category).toUpperCase()} - Daily Report`,
+        text: `${JSON.stringify(tweetData, null, 4)}.`
     };
-    transporter.sendMail(mailOptions, (error, info) => {
+    const transporter = await nodemailer.createTransport({
+        service: 'gmail',
+        port: 465,
+        secure: true,
+        auth: {
+            user: mailFrom,
+            pass: mailPass,
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    const mailInfo = await transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.log('error : transporter :', error);
         } else {
-            console.log(`Email sent to ${user.email}`);
+            console.log(`Email sent to ${user.email} - ${nodemailer.getTestMessageUrl(info)}`);
         }
     });
 }
 
-// Retrieve the users from the MongoDB collection and send them the weather data via email
-const sendWeatherEmails = () => {
-    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+// Retrieve the users from the MongoDB collection and send them the tweetData data via email
+const sendNewsletterEmails = () => {
+    MongoClient.connect(mongodb_url, { useNewUrlParser: true }, (err, client) => {
         if (err) throw err;
         const db = client.db(dbName);
         db.collection(collectionName).find({ deletedAt: { $exists: false } }).toArray((err, users) => {
             if (err) throw err;
-            users.forEach(user => {
+            users.forEach(async (user) => {
                 console.log('user - ', user)
-                sendWeatherEmail(user);
+                await sendNewsletterEmail(user);
             });
             client.close();
         });
     });
 }
 
-// Schedule the task to run every day at 9am
-const job = new cron.CronJob('* */1 * * * *', sendWeatherEmails, null, true);
+// Schedule the cron
+const job = new cron.CronJob('*/1 * * * * *', sendNewsletterEmails, null, false);
 job.start();
